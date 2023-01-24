88cfc091d3f09ee3a8556aee10378f58b4112d65
pascal029 <pascalvirgiawan05@gmail.com>
2023-01-24 11:57:13 +0700

    add page user

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,86 +1,84 @@
 <script>
-import Navbar from './components/Navbar.vue'
-import Home from './views/Home.vue'
-import Login from './views/Login.vue'
-import User from './views/User.vue'
-import List from './views/List.vue'
+import Navbar from "./components/Navbar.vue";
+import Home from "./views/Home.vue";
+import Login from "./views/Login.vue";
+import User from "./views/User.vue";
+import List from "./views/List.vue";
 export default {
-  components : {
+  components: {
     Navbar,
     Home,
     Login,
     User,
-    List
+    List,
   },
-  data(){
+  data() {
     return {
-      currentPage : 'login',
-      username : 'tes@eci.co.id',
-      password : 'tes123'
+      currentPage: "login",
+      username: "tes@eci.co.id",
+      password: "tes123",
+    };
+  },
+  created() {
+    if (localStorage.access_token) {
+      this.username = localStorage.username;
+      this.currentPage = "home";
+    } else {
+      this.currentPage = "login";
     }
   },
-  created () {
-    if(localStorage.access_token){
-      this.username = localStorage.username
-      this.currentPage = 'home'
-    }else {
-      this.currentPage='login'
-    }
-  },
-  methods : {
+  methods: {
     changePage(nextPage) {
-      this.currentPage = nextPage
+      this.currentPage = nextPage;
     },
-    async login(data){
+    async login(data) {
       try {
-        let error = ''
-        if(!data.password || !data.username) {
-          error = 'Username and password are required'
-          throw error
+        let error = "";
+        if (!data.password || !data.username) {
+          error = "Username and password are required";
+          throw error;
         }
-        if(data.username == this.username && data.password == this.password){
-          localStorage.setItem('access_token', 'ada')
-          localStorage.setItem('username', this.username)
-          Swal.fire('login success')
-          this.changePage("home")
+        if (data.username == this.username && data.password == this.password) {
+          localStorage.setItem("access_token", "ada");
+          localStorage.setItem("username", this.username);
+          Swal.fire("login success");
+          this.changePage("home");
         } else {
-          error = 'Incorrect username or password'
-          throw error
+          error = "Incorrect username or password";
+          throw error;
         }
       } catch (error) {
         Swal.fire({
-          position: 'center',
-          width : '15em',
-          icon: 'error',
+          position: "center",
+          width: "15em",
+          icon: "error",
           title: error,
           showConfirmButton: false,
-          timer: 1500
-        })
+          timer: 1500,
+        });
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <template>
   <div class="h-screen">
     <section v-if="currentPage == 'home'">
-    <Home />
-  </section>
-  <section v-if="currentPage == 'login'">
-    <Login @login="login" />
-  </section>
-  <section v-if="currentPage == 'list'">
-    <List />
-  </section>
+      <Home />
+    </section>
+    <section v-if="currentPage == 'login'">
+      <Login @login="login" />
+    </section>
+    <section v-if="currentPage == 'list'">
+      <List />
+    </section>
 
-  <section v-if="currentPage == 'user'">
-    <User />
-  </section>
-  <Navbar @changePage="changePage" />
+    <section v-if="currentPage == 'user'">
+      <User />
+    </section>
+    <Navbar @changePage="changePage" v-if="currentPage != 'login'" />
   </div>
-  
 </template>
 
-<style>
-</style>
+<style></style>
--- client/src/views/User.vue
+++ client/src/views/User.vue
@@ -1,5 +1,92 @@
+<script>
+export default {
+  data() {
+    return {
+      disabled: 0,
+      name: "tes",
+      username: "tes1",
+      email: "tes@eci.co.id",
+      password: "tes123",
+    };
+  },
+  methods: {
+    tesTriger(e) {
+      this.disabled == 0 ? (this.disabled += 1) : (this.disabled -= 1);
+    },
+    submitEdit() {
+      this.disabled -= 1;
+    },
+  },
+};
+</script>
+
 <template>
-    <div>
-        ini user
+  <div class="container px-6 h-full">
+    <div class="grid h-48 mt-10">
+      <div class="flex justify-center">
+        <span id="iconUser"><i class="fa-regular fa-user"></i></span>
+      </div>
+      <div>
+        <form @submit.prevent="submitEdit">
+          <div>
+            <input
+              type="text"
+              placeholder="Name"
+              class="input input-bordered w-full max-w-xs"
+              :disabled="disabled == 0"
+              v-model="name"
+            />
+          </div>
+          <div class="mt-2">
+            <input
+              type="text"
+              placeholder="Username"
+              class="input input-bordered w-full max-w-xs"
+              :disabled="disabled == 0"
+              v-model="username"
+            />
+          </div>
+          <div class="mt-2">
+            <input
+              type="text"
+              placeholder="Email"
+              class="input input-bordered w-full max-w-xs"
+              :disabled="disabled == 0"
+              v-model="email"
+            />
+          </div>
+          <div class="mt-2">
+            <input
+              type="password"
+              placeholder="Password"
+              class="input input-bordered w-full max-w-xs"
+              :disabled="disabled == 0"
+              v-model="password"
+            />
+          </div>
+          <div class="flex ml-20">
+            <button
+              @click.prevent="tesTriger"
+              type="submit"
+              class="btn mt-2 mr-10"
+              id="editButton"
+            >
+              {{ disabled == 0 ? "EDIT" : "CANCEL" }}
+            </button>
+            <button type="submit" class="btn mt-2" id="submitButton">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
+  </div>
 </template>
+
+<style scoped>
+#iconUser {
+  font-size: 10em;
+  color: #000000;
+  margin-left: 10px;
+}
+</style>
